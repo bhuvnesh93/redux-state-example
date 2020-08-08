5c33463852b38b37e1c82865719d2579be727c73
Bhuvneshwar Garg <garg.bhuvnesh93@gmail.com>
2020-08-08 17:18:39 +0530

    Remove Unused Code

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
 import { AppRegistry } from 'react-native';
-// import { Provider } from 'react-redux';
-// import React from 'react';
 import { name as appName } from './app.json';
 import App from './app/App';
-// import configureStore from './lib/store';
-
-// const store = configureStore();
-
-// const RNRedux = () => (
-//   <Provider store={store}>
-//     <App />
-//   </Provider>
-// );
 
 AppRegistry.registerComponent(appName, () => App);
